1a577d64a6d8eae55e6290a9b499d3e0ecec9c53
yuyutti <yuyutti@yuyutti.com>
2024-01-06 18:23:02 +0900

    commits

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ echo "y" | sudo ufw enable
 sudo wg genkey | sudo tee /etc/wireguard/privatekey | wg pubkey | sudo tee /etc/wireguard/publickey
 
 # クライアント設定を生成
-sudo wg genkey | sudo tee /opt/client_privatekey | wg pubkey | sudo tee /opt/client_publickey
+sudo wg genkey | sudo tee /etc/wireguard/client_privatekey | wg pubkey | sudo tee /etc/wireguard/client_publickey
 
 # 事前共有キーを生成
 sudo wg genkey | sudo tee /etc/wireguard/client_preshared.key
@@ -33,7 +33,7 @@ PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o 
 PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 
 [Peer]
-PublicKey = $(cat /opt/client_publickey)
+PublicKey = $(cat /etc/wireguard/client_publickey)
 PresharedKey = $(cat /etc/wireguard/client_preshared.key)
 AllowedIPs = 172.16.0.1/32
 " > /etc/wireguard/wg0.conf'
@@ -41,7 +41,7 @@ AllowedIPs = 172.16.0.1/32
 # wireguardクライアントインターフェースの設定
 sudo sh -c 'echo "
 [Interface]
-PrivateKey = $(cat /opt/client_privatekey)
+PrivateKey = $(cat /etc/wireguard/client_privatekey)
 Address = 10.0.0.2/24
 DNS = 1.1.1.1
 
